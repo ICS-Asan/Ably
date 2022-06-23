04e5d3f15d807dd2201485cf7e9f53eae5a3c9db
ICS-Asan <carpediem1015@gmail.com>
2022-06-23 17:15:42 +0900

    feat: HTTPNetworkError 생성 #2
    - Error처리를 위한 enum 생성 및 적용

--- Ably/Ably/Data/Network/HttpNetwork.swift
+++ Ably/Ably/Data/Network/HttpNetwork.swift
@@ -9,17 +9,16 @@ class HttpNetwork {
         self.urlSession = urlSession
     }
     
-    private func fetch(with endPoint: URL?) -> Observable<Data> {
+    func fetch(with endPoint: URL?) -> Observable<Data> {
         guard let endPoint = endPoint else {
-            // 유효하지 않은 URL Error 추가
-            return .error(fatalError())
+            return .error(HTTPNetworkError.invalidEndPoint)
         }
 
         return Observable.create() { [weak self] emitter in
             let urlRequest = URLRequest(url: endPoint, method: .get)
-            let task = self?.urlSession.dataTask(with: urlRequest) { (data, _ , error) in
+            let task = self?.urlSession.dataTask(with: urlRequest) { (data, response , error) in
                 guard error == nil else {
-                    emitter.onError(error!)
+                    emitter.onError(HTTPNetworkError.invalidRequest)
                     return
                 }
 
