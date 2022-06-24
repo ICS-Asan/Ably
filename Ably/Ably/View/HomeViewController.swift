961f85b23890050681a80ebf3753ea8cd77479fd
ICS-Asan <carpediem1015@gmail.com>
2022-06-24 22:19:49 +0900

    feat: createCollectionViewLayout() 구현 #8
    - 내부에서 사용되는 메서드 구현
    - 각 section에 맞는 layout을 리턴하는 함수로 구현

--- Ably/Ably/View/HomeViewController.swift
+++ Ably/Ably/View/HomeViewController.swift
@@ -8,11 +8,69 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    enum Section {
+        case banners
+        case goods
+        
+        var index: Int {
+            switch self {
+            case .banners:
+                return 0
+            case .goods:
+                return 1
+            }
+        }
+    }
+    
+    private var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         navigationItem.title = Design.Text.homeViewTitle
+    }
+    
+}
+
+extension HomeViewController {
+    func createCollectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+                                                            layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            switch sectionIndex {
+            case Section.banners.index:
+                return self.creatGoodsSectionLayout()
+            case Section.goods.index:
+                return self.creatGoodsSectionLayout()
+            default:
+                return nil
+            }
+        }
+        return layout
+    }
+    
+    private func creatBannerSectionLayout() -> NSCollectionLayoutSection {
+        let itemsize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .estimated(400))
+        let item = NSCollectionLayoutItem(layoutSize: itemsize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: itemsize.widthDimension, heightDimension: itemsize.heightDimension)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .paging
+        
+        return section
+    }
+    
+    private func creatGoodsSectionLayout() -> NSCollectionLayoutSection {
+        let itemsize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .estimated(250))
+        let item = NSCollectionLayoutItem(layoutSize: itemsize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: itemsize.widthDimension,
+                                               heightDimension: itemsize.heightDimension)
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 1)
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
     }
 
 }
+
