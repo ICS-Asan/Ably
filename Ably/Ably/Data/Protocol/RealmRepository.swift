295094774a1500f861929fc4dda49c979ab1d806
ICS-Asan <carpediem1015@gmail.com>
2022-06-26 21:31:02 +0900

    style: RealmRepository 코딩 컨벤션 수정 #19

--- Ably/Ably/Data/Protocol/RealmRepository.swift
+++ Ably/Ably/Data/Protocol/RealmRepository.swift
@@ -2,10 +2,7 @@ import Foundation
 import RxSwift
 
 protocol RealmRepository {
-    
     func fetchRealmData() -> Observable<[AblyGoods]>
-
     func append(ablyGoods: AblyGoods)
-    
     func delete(ablyGoods: AblyGoods) 
 }
