fb79597fbe8c5007c20404c0523fb185b3c8a43d
ICS-Asan <carpediem1015@gmail.com>
2022-06-26 22:11:08 +0900

    file: ViewModelDescribing 파일 생성 및 코드 위치 변경 #19

--- Ably/Ably/View/HomeView/HomeViewModel.swift
+++ Ably/Ably/View/HomeView/HomeViewModel.swift
@@ -1,13 +1,6 @@
 import Foundation
 import RxSwift
 
-protocol ViewModelDescribing {
-    associatedtype Input
-    associatedtype Output
-    
-    func transform(_ input: Input) -> Output
-}
-
 class HomeViewModel {
     private let homeDataUseCase = AblyHomeDataUseCase()
     private let disposeBag: DisposeBag = .init()
