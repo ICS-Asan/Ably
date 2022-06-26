e9d1f3e00bc3aaeb9f24e008437b14ed13b3d922
ICS-Asan <carpediem1015@gmail.com>
2022-06-26 22:09:25 +0900

    style: ViewController 코딩 컨벤션 및 접근제어자 수정 #19

--- Ably/Ably/View/FavoriteView/FavoriteViewController.swift
+++ Ably/Ably/View/FavoriteView/FavoriteViewController.swift
@@ -3,7 +3,7 @@ import RxSwift
 
 class FavoriteViewController: UIViewController {
     
-    enum Section {
+    private enum Section {
         case favorite
     }
     
@@ -13,7 +13,7 @@ class FavoriteViewController: UIViewController {
     private let disposeBag: DisposeBag = .init()
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = Design.Text.favoritViewTitle
+        setupNavigationBar()
         setupFavoriteCollectionView()
         fetchFavoriteGoods()
     }
@@ -23,13 +23,25 @@ class FavoriteViewController: UIViewController {
         fetchFavoriteGoods()
     }
     
-    func fetchFavoriteGoods() {
+    private func setupNavigationBar() {
+        navigationItem.title = Design.Text.favoritViewTitle
+    }
+    
+    private func fetchFavoriteGoods() {
         viewModel.fetchFavoriteGoods()
             .observe(on: MainScheduler.instance)
             .subscribe(onNext: { [weak self] goods in
                 self?.populate(goods: goods)
             })
             .disposed(by: disposeBag)
+    }
+    
+    private func populate(goods: [AblyGoods]?) {
+        guard let goods = goods else { return }
+        var snapshot = NSDiffableDataSourceSnapshot<Section, AblyGoods>()
+        snapshot.appendSections([.favorite])
+        snapshot.appendItems(goods, toSection: .favorite)
+        dataSource?.apply(snapshot)
     }
 }
 
@@ -44,7 +56,7 @@ extension FavoriteViewController {
         setupCollectionViewConstraints()
     }
     
-    func createCollectionViewLayout() -> UICollectionViewLayout {
+    private func createCollectionViewLayout() -> UICollectionViewLayout {
         let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
                                                             layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
             return self.creatFavoriteSectionLayout()
@@ -84,13 +96,5 @@ extension FavoriteViewController {
         favoriteCollectionView.snp.makeConstraints { make in
             make.edges.equalTo(view.safeAreaLayoutGuide)
         }
-    }
-    
-    private func populate(goods: [AblyGoods]?) {
-        guard let goods = goods else { return }
-        var snapshot = NSDiffableDataSourceSnapshot<Section, AblyGoods>()
-        snapshot.appendSections([.favorite])
-        snapshot.appendItems(goods, toSection: .favorite)
-        dataSource?.apply(snapshot)
     }
 }
