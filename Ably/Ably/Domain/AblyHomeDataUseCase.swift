7ff588c1dc7670a986c6d674d1a2f83cc5012999
ICS-Asan <carpediem1015@gmail.com>
2022-06-26 19:09:15 +0900

    feat: realmRepository UseCase에 적용 #13

--- Ably/Ably/Domain/AblyHomeDataUseCase.swift
+++ Ably/Ably/Domain/AblyHomeDataUseCase.swift
@@ -3,9 +3,14 @@ import RxSwift
 
 class AblyHomeDataUseCase {
     let ablyHomeDataRepository: AblyAPIRepository
+    let realmRepository: RealmRepository
     
-    init(ablyHomeDataRepository: AblyAPIRepository = AblyHomeDataRepository()) {
+    init(
+        ablyHomeDataRepository: AblyAPIRepository = AblyHomeDataRepository(),
+        realmRepository: RealmRepository = RealmRepository()
+    ) {
         self.ablyHomeDataRepository = ablyHomeDataRepository
+        self.realmRepository = realmRepository
     }
     
     func fetchAblyHomeData() -> Observable<AblyHomeData> {
@@ -14,5 +19,17 @@ class AblyHomeDataUseCase {
     
     func fetchAblyGoodsForPagination(with lastId: Int) -> Observable<AblyHomeData> {
         return ablyHomeDataRepository.fetchAblyGoodsForPaging(with: lastId)
+    }
+    
+    func fetchRealmData() -> Observable<[AblyGoods]> {
+        return realmRepository.fetchRealmData()
+    }
+    
+    func appendToRealm(ablyGoods: AblyGoods) {
+        realmRepository.append(ablyGoods: ablyGoods)
+    }
+    
+    func deleteFromRealm(ablyGoods: AblyGoods) {
+        realmRepository.delete(ablyGoods: ablyGoods)
     }
 }
