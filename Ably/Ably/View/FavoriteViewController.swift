de1fd26f6a678e5707437dfdc5fb98319798d668
ICS-Asan <carpediem1015@gmail.com>
2022-06-26 15:49:00 +0900

    feat: favoriteCollectionView생성 및 layout 설정 #11

--- Ably/Ably/View/FavoriteViewController.swift
+++ Ably/Ably/View/FavoriteViewController.swift
@@ -1,8 +1,49 @@
 import UIKit
+import RxSwift
 
 class FavoriteViewController: UIViewController {
     
+    enum Section {
+        case favorite
+    }
+    
+    private var favoriteCollectionView: UICollectionView!
+    private var dataSource: UICollectionViewDiffableDataSource<Section, AblyGoods>?
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension FavoriteViewController {
+    private func setupFavoriteCollectionView() {
+        favoriteCollectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: createCollectionViewLayout()
+        )
+        registerCollectionViewCell()
+    }
+    
+    func createCollectionViewLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int,
+                                                            layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            return self.creatFavoriteSectionLayout()
+        }
+        return layout
+    }
+    
+    private func creatFavoriteSectionLayout() -> NSCollectionLayoutSection {
+        let itemsize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                              heightDimension: .estimated(150))
+        let item = NSCollectionLayoutItem(layoutSize: itemsize)
+        let groupSize = NSCollectionLayoutSize(widthDimension: itemsize.widthDimension,
+                                               heightDimension: itemsize.heightDimension)
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 1)
+        let section = NSCollectionLayoutSection(group: group)
+        
+        return section
+    }
+    
+    private func registerCollectionViewCell() {
+        favoriteCollectionView.register(GoodsCell.self)
     }
 }
