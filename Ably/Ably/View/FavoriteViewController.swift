942ab8b730f1cca5c93bdd2c19ca4d44453ed277
ICS-Asan <carpediem1015@gmail.com>
2022-06-26 16:42:53 +0900

    feat: FavoriteViewController TabBarController에 할당 #11

--- Ably/Ably/View/FavoriteViewController.swift
+++ Ably/Ably/View/FavoriteViewController.swift
@@ -12,6 +12,7 @@ class FavoriteViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = Design.Text.favoritViewTitle
+        setupFavoriteCollectionView()
     }
 }
 
